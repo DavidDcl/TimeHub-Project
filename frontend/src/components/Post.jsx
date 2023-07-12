5d7f3c9df32a26d45b785f6f9b364e690eb25f3d
Killian Cousseau <killian.cousseau@gmail.com>
2023-07-12 15:42:03 +0200

    Post

--- frontend/src/components/Post.jsx
+++ frontend/src/components/Post.jsx
@@ -1,5 +1,21 @@
 const Post = () => {
-  return <div>Post</div>;
+  return (
+    <div className="flex flex-col">
+      <div className="flex justify-around">
+        <img src="" alt="" />
+        <div className="flex justify-around">
+          <p>Cleopatre</p>
+          <p>Date</p>
+        </div>
+      </div>
+      <p>Post Content</p>
+      <div>
+        <button>Commentaires</button>
+        <button>Like</button>
+        <button>Dislike</button>
+      </div>
+    </div>
+  );
 };
 
 export default Post;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,12 @@
+import Post from "../components/Post";
+
 const Home = () => {
   return (
     <div>
       <h1>Liste de Posts</h1>
+      <Post />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
