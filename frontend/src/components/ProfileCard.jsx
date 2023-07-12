eefd50f91a03802b7421a935b3a94d63186d47e5
Nawaksam <sam.cardonnel@gmail.com>
2023-07-12 21:56:47 +0200

    ajusting profilecard

--- frontend/src/components/ProfileCard.jsx
+++ frontend/src/components/ProfileCard.jsx
@@ -2,13 +2,13 @@ import React from "react"
 
 export default function ProfileCard({ user }) {
   return (
-    <div className="hover:scale-105 flex gap-5 border-2 border-secondary rounded-[2rem] p-4">
+    <div className="hover:bg-slate-800 flex gap-5 border-2 border-secondary rounded-[2rem] p-1.5">
       <img
         src={`/${user.id}.png`}
         alt={`${user.firstname}`}
         className="border-4 border-primary rounded-full "
       />
-      <div className="flex flex-col justify-center dark:text-secondary">
+      <div className="flex flex-col justify-center  gap-2 dark:text-secondary text-xl ">
         <div>
           {user.firstname} {user.lastname}
         </div>
