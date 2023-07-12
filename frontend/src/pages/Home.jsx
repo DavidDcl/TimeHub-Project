68feb00253e9952a6fb66bc4b5eb325e9b7017cf
Killian Cousseau <killian.cousseau@gmail.com>
2023-07-12 16:38:01 +0200

    Post

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,18 @@
+import { useEffect, useState } from "react";
 import Post from "../components/Post";
 
 const Home = () => {
+  const [posts, setPosts] = useState(null);
+  useEffect(() => {
+    fetch("http://localhost:8000/posts")
+      .then((res) => res.json())
+      .then((data) => setPosts(data));
+  }, []);
+  console.log(posts);
   return (
     <div>
       <h1>Liste de Posts</h1>
-      <Post />
+      {posts && posts.map((post) => <Post key={post.id} post={post} />)}
     </div>
   );
 };
