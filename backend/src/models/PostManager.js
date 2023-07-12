ede5ace56460fc75cdba945ff3982e5f2cea89b7
Nawaksam <sam.cardonnel@gmail.com>
2023-07-12 17:48:29 +0200

    removing post title

--- backend/src/models/PostManager.js
+++ backend/src/models/PostManager.js
@@ -7,21 +7,21 @@ class PostManager extends AbstractManager {
 
   findAll() {
     return this.database.query(
-      `SELECT p.id, p.title, p.content, u.firstname, u.lastname, u.picture FROM ${this.table} p JOIN users u ON  p.author = u.id`
+      `SELECT p.id, p.content, u.firstname, u.lastname, u.picture FROM ${this.table} p JOIN users u ON  p.author = u.id`
     )
   }
 
   find(id) {
     return this.database.query(
-      `SELECT p.id, p.title, p.content, u.firstname, u.lastname, u.picture FROM ${this.table} p JOIN users u ON  p.author = u.id where p.id = ?`,
+      `SELECT p.id, p.content, u.firstname, u.lastname, u.picture FROM ${this.table} p JOIN users u ON  p.author = u.id where p.id = ?`,
       [id]
     )
   }
 
   insert(item) {
     return this.database.query(
-      `INSERT INTO ${this.table} (title, content, author) values (?,?,1)`,
-      [item.title, item.content]
+      `INSERT INTO ${this.table} (content, author) values (?,1)`,
+      [item.content]
     )
   }
 
