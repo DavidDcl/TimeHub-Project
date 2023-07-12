c33280ba982071c6ab3e025ce0f2d3a26c01b00b
Nawaksam <sam.cardonnel@gmail.com>
2023-07-12 21:48:26 +0200

    link on search page

--- frontend/src/components/ProfileCard.jsx
+++ frontend/src/components/ProfileCard.jsx
@@ -1,19 +1,22 @@
 import React from "react"
+import { Link } from "react-router-dom"
 
 export default function ProfileCard({ user }) {
   return (
-    <div className="hover:scale-105 flex gap-5 border-2 border-secondary rounded-[2rem] p-4">
-      <img
-        src={`/${user.id}.png`}
-        alt={`${user.firstname}`}
-        className="border-4 border-primary rounded-full "
-      />
-      <div className="flex flex-col justify-center dark:text-secondary">
-        <div>
-          {user.firstname} {user.lastname}
+    <Link to={`/profile/${user.id}`}>
+      <div className="hover:scale-105 flex gap-5 border-2 border-secondary rounded-[2rem] p-4">
+        <img
+          src={`/${user.id}.png`}
+          alt={`${user.firstname}`}
+          className="border-4 border-primary rounded-full "
+        />
+        <div className="flex flex-col justify-center dark:text-secondary">
+          <div>
+            {user.firstname} {user.lastname}
+          </div>
+          <div>@{user.nickname}</div>
         </div>
-        <div>@{user.nickname}</div>
       </div>
-    </div>
+    </Link>
   )
 }
