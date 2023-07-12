ecefabb681ebd273ed9d32b040b25bb950193f58
Nawaksam <sam.cardonnel@gmail.com>
2023-07-12 20:14:37 +0200

    begining search bar

--- frontend/src/pages/SearchPage.jsx
+++ frontend/src/pages/SearchPage.jsx
@@ -1,5 +1,31 @@
-const SearchPage = () => {
-  return <div>SearchPage</div>;
-};
+import { useState } from "react"
 
-export default SearchPage;
+const SearchPage = () => {
+  const [input, setInput] = useState("")
+
+  const handleSearch = () => {}
+  return (
+    <div className="min-h-[80vh] flex flex-col items-center justify-between">
+      <div className="flex justify-center mt-1">
+        <div className="join">
+          <input
+            className="input input-bordered join-item dark:text-secondary"
+            placeholder="Par ici..."
+            type="text"
+            onChange={(e) => {
+              setInput(e.target.value)
+            }}
+          />
+          <button
+            className="bg-secondary dark:text-base-100 btn join-item rounded-r-full"
+            onClick={handleSearch}
+          >
+            Chercher
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default SearchPage
